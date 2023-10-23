a4e338ca7ef22080c2f5ee8f763b51fdacffc472
Emmanuel Okon <emmaudo27@gmail.com>
2023-10-23 04:52:49 +0100

    header and topnav component

---
+++ components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TopNav from "./TopNav";
+import Header from "./Header";
+
+const Navbar = () => {
+  return (
+    <>
+      <Header />
+      <TopNav />
+    </>
+  );
+};
+
+export default Navbar;
